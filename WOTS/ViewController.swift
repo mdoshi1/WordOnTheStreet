8b0e9c10ab11cfcffc56c85e301d8a20f4354a00
MC_Doshi <doshi.michael@gmail.com>
2017-04-29 13:30:54 -0700

    Initial Commit

---
+++ WOTS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WOTS
+//
+//  Created by Michael-Anthony Doshi on 4/29/17.
+//  Copyright © 2017 Learning Curve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
