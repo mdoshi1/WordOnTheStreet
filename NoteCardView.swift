f40e8cec7bc5233bc7cc7918ea143010e7567e50
Max Freundlich <maxwfreu@stanford.edu>
2017-04-29 18:01:36 -0700

    Changed textviews to uilabels

--- NoteCardView.swift
+++ NoteCardView.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class NoteCardView: UIView {
     
-    lazy var wordView: UITextView = {
-        let wordView = UITextView(frame:  CGRect(x: self.frame.width/2 - 100/2, y: 7, width: 100, height: 100))
-        wordView.font = .systemFont(ofSize: 24)
-        wordView.backgroundColor = nil;
+    lazy var wordView: UILabel = {
+        let wordView = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        wordView.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2-20)
+        wordView.textAlignment = .center
+        wordView.font = .systemFont(ofSize: 40)
         return wordView
     }()
     
-    lazy var translationView: UITextView = {
-        let translationView = UITextView(frame:  CGRect(x: self.frame.width/4, y: 50, width: self.frame.width, height: 100))
-        translationView.font = .systemFont(ofSize: 20)
-        translationView.backgroundColor = nil
+    lazy var translationView: UILabel = {
+        let translationView = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        translationView.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2+20)
+        translationView.textAlignment = .center
+        translationView.font = .systemFont(ofSize: 30)
         return translationView
     }()
     
@@ -30,6 +32,7 @@ class NoteCardView: UIView {
         image.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
         addSubview(image)
         addSubview(wordView)
+        translationView.isHidden = true;
         addSubview(translationView);
     }
 
